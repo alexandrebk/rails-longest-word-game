4024f762b34250e0274353e89be6f7a33e36794a
Alexandre Bouvier <alexandre.b2506@gmail.com>
2021-04-12 17:40:04 +0200

    correction

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,45 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = generate_grid(10)
+  end
+
+  def score
+
+    puts "SCORE METHOD"
+    p @letters = params[:letters].split('')
+    p @answer = params[:answer]
+    puts "-------"
+    @message = score_and_message(@answer, @letters)
+  end
+
+  private
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a.sample }
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def score_and_message(attempt, grid)
+    if included?(attempt.upcase, grid)
+      if english_word?(attempt)
+        "well done"
+      else
+        "not an english word"
+      end
+    else
+      "not in the grid"
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
+
